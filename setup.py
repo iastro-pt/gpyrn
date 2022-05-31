147d942b929ee16253fbc58e8e6305ef07d9c405
João Faria <joao.faria@astro.up.pt>
2022-05-31 14:41:14 +0100

    test publish action

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 
 setup(
     name='gpyrn',
-    version='1.0',
-    description='Gaussian processes regression networks for exoplanet detection',
+    version='1.0.1',
+    description='Gaussian process regression networks for exoplanet detection',
     author='João Camacho',
     author_email='joao.camacho@astro.up.pt',
     license='MIT',
