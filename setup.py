57eced3264108b11adde7f4966d222d0323ae7d9
João Faria <joao.faria@astro.up.pt>
2022-05-31 14:48:39 +0100

    test upload to pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,19 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+
+# Get the long description from the README file
+from os import path
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='gpyrn',
     version='1.0.1',
     description='Gaussian process regression networks for exoplanet detection',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='João Camacho',
     author_email='joao.camacho@astro.up.pt',
     license='MIT',
